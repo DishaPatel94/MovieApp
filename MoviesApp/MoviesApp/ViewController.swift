875b833d96ae43a20525e5786f97c312db3bcb34
MAC110 <test@gmail.com>
2019-01-08 18:29:31 +0530

    Initial commit

---
+++ MoviesApp/MoviesApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MoviesApp
+//
+//  Created by MAC110 on 1/8/19.
+//  Copyright © 2019 MAC110. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
